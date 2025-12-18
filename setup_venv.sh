2e551d5ea295dbe301feca0bddd091f2da498007
Lukasz Kujawiak <lukasz.kujawiak@gmail.com>
2025-12-18 09:49:23 +0100

    venv

--- setup_venv.sh
+++ setup_venv.sh
@@ -5,6 +5,3 @@ python3 -m venv venv
 
 echo "Activating virtual environment..."
 source venv/bin/activate
-
-echo "Upgrading pip, setuptools, and wheel..."
-pip install --upgrade pip setuptools wheel
